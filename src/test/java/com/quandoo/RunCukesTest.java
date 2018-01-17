ed90ec39380088b7aa1bb8f08413ff9a2bae967d
Violetta Abramova <violetta.abramova@storecast.de>
2018-01-17 11:49:12 +0100

    Add pretty plugin for Cucumber

--- src/test/java/com/quandoo/RunCukesTest.java
+++ src/test/java/com/quandoo/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "json:target/cucumber-report.json", glue = "com.quandoo")
+@CucumberOptions(plugin = {"json:target/cucumber-report.json", "pretty"}, glue = "com.quandoo")
 public class RunCukesTest {
 }
