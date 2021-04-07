86edad7fbda4cf14384393e4636446e28d1dc5c3
Violeta <violeta.abramova@quandoo.com>
2021-04-07 20:12:35 +0200

    add docker compose

--- src/test/java/de/sconto/RunCukesTest.java
+++ src/test/java/de/sconto/RunCukesTest.java
@@ -1,8 +1,17 @@
 package de.sconto;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverProvider;
+import io.cucumber.java8.En;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -10,5 +19,15 @@ import org.junit.runner.RunWith;
         glue = "de.sconto",
         strict = true
 )
-public class RunCukesTest {
+public class RunCukesTest implements En {
+    public RunCukesTest() {
+
+        Before(() -> {
+            Configuration.remote = "http://localhost:4445/wd/hub";
+            Configuration.browserCapabilities.setCapability("enableVNC", true);
+            Configuration.browserCapabilities.setCapability("enableVideo", true);
+            Configuration.browser = "chrome";
+            Configuration.browserVersion = "71.0";
+        });
+    }
 }
